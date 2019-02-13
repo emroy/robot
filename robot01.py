2644a20702dd929a4e56aec5989e1138a130057c
Ryan Philbert <rphilbert@mppeuct.gob.ve>
2019-02-13 13:29:22 -0400

    comienzo del proyecto robot

---
+++ robot01.py
@@ -0,0 +1,40 @@
+import pyautogui as ui
+import time
+import os
+
+result = input('Comenzar, [seguir,noseguir]?')
+
+if result =='seguir' :
+    os.chdir('c:\wamp\www')
+    madda = True
+    
+    while madda == True:
+        
+        seguir = ui.locateCenterOnScreen('follow.png')
+        if seguir == None:
+            ui.scroll(-200, pause=1.)
+            
+        if seguir != None:
+            x, y = seguir
+            ui.moveTo(x,y,.5)
+            ui.doubleClick()
+elif result == 'noseguir':
+    os.chdir('c:\wamp\www')
+    madda = True
+    
+    while madda == True:
+        
+        seguir = ui.locateCenterOnScreen('following.png')
+        if seguir == None:
+            ui.scroll(-200, pause=1.)
+        elif seguir != None:
+            x, y = seguir
+            ui.moveTo(x,y,.5)
+            ui.click()
+            time.sleep(1)
+            confirm = ui.locateCenterOnScreen('confirmation.png')
+            print(confirm)
+            if confirm != None:
+                x, y = confirm
+                ui.moveTo(x,y,.5)
+                ui.doubleClick()
