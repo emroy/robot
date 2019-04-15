1f4dd032535c4be94815d504218dadb202b58f2d
Ryan Philbert <snavarro169@gmail.com>
2019-04-15 15:59:49 +0100

    first version windows finished

--- robot01.py
+++ robot01.py
@@ -2,39 +2,54 @@ import pyautogui as ui
 import time
 import os
 
+#Default configs
+ui.FAILSAFE = True
+
+def wait(secs):
+    time.sleep(secs)
+
+#Click de seguir a las personas
+def clickFollow():
+    try:
+        x, y = ui.locateCenterOnScreen('follow.png')
+        ui.moveTo(x, y, .5)
+        ui.doubleClick()
+        wait(1)
+    except:
+        print('Scrolling for more Follows')
+        ui.scroll(-200, pause=1.)
+
+#Click de dejar de seguir
+def clickUnfollow():
+    try:
+        x, y  = ui.locateCenterOnScreen('following.png')
+        ui.moveTo(x,y,.5)
+        ui.click()
+        wait(1)
+    except:
+        print('Scrolling for more Unfollow')
+        ui.scroll(-200, pause=1.)
+
+#Click de confirmar dejar de seguir
+def confirmUnfollow():
+    try:
+        x, y = ui.locateCenterOnScreen('confirmation.png')
+        ui.moveTo(x,y,.5)
+        ui.click()
+        wait(1)
+    except:
+        print('No confirmation popup shown, looking for more unfollows')
+        clickUnfollow()
+
+
 result = input('introduzca una opcion [seguir, noseguir] \n ')
 
+
 if result =='seguir' :
-    #os.chdir('c:\wamp\www')
-    madda = True
-    
-    while madda == True:
-        
-        seguir = ui.locateCenterOnScreen('follow.png')
-        if seguir == None:
-            ui.scroll(-200, pause=1.)
-            
-        if seguir != None:
-            x, y = seguir
-            ui.moveTo(x,y,.5)
-            ui.doubleClick()
+    while True:
+        clickFollow()
 elif result == 'noseguir':
-    #os.chdir('c:\wamp\www')
-    madda = True
-    
-    while madda == True:
-        
-        seguir = ui.locateCenterOnScreen('following.png')
-        if seguir == None:
-            ui.scroll(-200, pause=1.)
-        elif seguir != None:
-            x, y = seguir
-            ui.moveTo(x,y,.5)
-            ui.click()
-            time.sleep(1)
-            confirm = ui.locateCenterOnScreen('confirmation.png')
-            print(confirm)
-            if confirm != None:
-                x, y = confirm
-                ui.moveTo(x,y,.5)
-                ui.doubleClick()
+    while True:
+        clickUnfollow()
+        confirmUnfollow()
+            
